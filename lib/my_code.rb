73340e9974851953da137267095e956cadffc515
Kenny Marks <kmarks2013@gmail.com>
2019-08-01 03:01:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -51,14 +51,4 @@ def reduce_to_total(source_array, starting_point = 0)
   total
 end
 
-def reduce_to_all_true(source_array)
-  i = 0 
-  while i < source_array.length do
-    if source_array[i] = true 
-    return true
-    i += 1
-    elsif source_array[i] = false 
-    return false
-    end
-  end
-end
+
